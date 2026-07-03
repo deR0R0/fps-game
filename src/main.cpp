155c0450a89b7833f68c0f4abeaf34ff7dccab60
deR0R0 <rorozee1@gmail.com>
2026-07-02 21:09:48 -0400

    feat: get a test triangle rendering working

--- src/main.cpp
+++ src/main.cpp
@@ -26,10 +26,44 @@ int main() {
 
     RenderLib::ShaderLoader::loadShaders();
 
+    // render triangle
+    float vertices[] = {-0.5f, -0.5f, 0.0f, 0.5f, -0.5f,
+                        0.0f,  0.0f,  0.5f, 0.0f};
+
+    unsigned int VBO, VAO;
+
+    glGenVertexArrays(1, &VAO); // generate before other buffers
+
+    // generate buffers for vertices
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+    glBindVertexArray(VAO);
+
+    // store vertex data
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float),
+                          (void *)0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
     while (!glfwWindowShouldClose(window)) {
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+
+    // cleanup vertex buffer stuff
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
 
     glfwTerminate();
     return 0;
